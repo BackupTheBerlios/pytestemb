eceef8d54abedba833c350524f40fbbb4375824c
octopy <octopy>
2009-02-03 22:39:37 +0000

    *** empty log message ***

---
+++ PyTestEmb/src/setup.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+from distutils.core import setup
+
+
+
+setup(name="pytestemb",
+      version="1.0",
+      author="octopy",
+      author_email="octopy@gmail.com",
+      url = "http://pytestemb.berlios.de",
+      license = "GPL",
+      packages=["pytestemb"])
